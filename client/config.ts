dc5f1c221ed9963f1a62446ec3ebb95885bb2b46
Ahmed Mahmoud <ahmedmahmoudkzm@gmail.com>
2025-07-09 20:42:27 +0300

    Remove config.ts from repo and ignore it

--- client/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-    PRODUCTION: true,
-    REACT_APP_API_URL_PROD: 'https://eight88-backend-service.onrender.com/',
-    REACT_APP_API_URL_DEV: 'http://192.168.1.4:3000/',
-}
-
-export default config
