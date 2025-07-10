365166b9cdd2c116b596c04e20bf07db88218a2a
Ahmed Mahmoud <ahmedmahmoudkzm@gmail.com>
2025-07-10 22:26:08 +0300

    Tracked config again

---
+++ client/config.ts
@@ -0,0 +1,7 @@
+const config = {
+    PRODUCTION: true,
+    REACT_APP_API_URL_PROD: 'https://eight88-backend-service.onrender.com/',
+    REACT_APP_API_URL_DEV: 'http://localhost:3000/',
+}
+
+export default config
