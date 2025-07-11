53d2d48b11bc1a6e70297d07dce7e67666692eb2
Ahmed Mahmoud <ahmedmahmoudkzm@gmail.com>
2025-07-11 21:30:44 +0300

    Added images to sitemap

--- api/routes/sitemapRoute.js
+++ api/routes/sitemapRoute.js
@@ -4,11 +4,31 @@ import ProductModel from '../models/productModel.js'
 
 const router = express.Router()
 
+function escapeXml(str) {
+    return str.replace(/[<>&'"]/g, (c) => {
+        switch (c) {
+            case '<':
+                return '&lt;'
+            case '>':
+                return '&gt;'
+            case '&':
+                return '&amp;'
+            case "'":
+                return '&apos;'
+            case '"':
+                return '&quot;'
+        }
+    })
+}
+
 router.get('/sitemap.xml', async (req, res) => {
     const baseUrl = 'https://www.splash-furniture.com'
-
     const today = new Date().toISOString()
-    const products = await ProductModel.find({}, '_id updatedAt')
+
+    const products = await ProductModel.find(
+        {},
+        '_id updatedAt imageUrls name description'
+    )
     const categories = await CategoryModel.find({}, '_id updatedAt')
 
     const staticRoutes = [
@@ -24,7 +44,8 @@ router.get('/sitemap.xml', async (req, res) => {
     ]
 
     let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`
-    xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`
+    xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n`
+    xml += `        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n`
 
     staticRoutes.forEach((path) => {
         let priority = '0.5'
@@ -35,7 +56,7 @@ router.get('/sitemap.xml', async (req, res) => {
             priority = '0.8'
         } else if (['/account', '/cart', '/wishlist'].includes(path)) {
             priority = '0.7'
-        } else if (['/forgot-password'].includes(path)) {
+        } else if (path === '/forgot-password') {
             priority = '0.6'
         } else if (['/login', '/sign-up'].includes(path)) {
             priority = '0.4'
@@ -67,7 +88,34 @@ router.get('/sitemap.xml', async (req, res) => {
         <loc>${baseUrl}/product/${prod._id}</loc>
         <lastmod>${lastmod}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.8</priority>
+        <priority>0.8</priority>`
+
+        if (Array.isArray(prod.imageUrls)) {
+            prod.imageUrls.forEach((imgPath) => {
+                // English
+                xml += `
+        <image:image>
+            <image:loc>${baseUrl}${imgPath}</image:loc>
+            <image:title>${escapeXml(prod.name || '')}</image:title>
+            <image:caption>${escapeXml(prod.description || '')}</image:caption>
+        </image:image>
+    `
+                // Arabic
+                if (prod.name_ar || prod.description_ar) {
+                    xml += `
+        <image:image>
+            <image:loc>${baseUrl}${imgPath}</image:loc>
+            <image:title>${escapeXml(prod.name_ar || '')}</image:title>
+            <image:caption>${escapeXml(
+                prod.description_ar || ''
+            )}</image:caption>
+        </image:image>
+        `
+                }
+            })
+        }
+
+        xml += `
     </url>`
     })
 
