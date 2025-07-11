ca8560c949ae70f41f80c16bb253ef7434d35f66
Ahmed Mahmoud <ahmedmahmoudkzm@gmail.com>
2025-07-11 20:47:52 +0300

    Added /seo route

--- api/app.js
+++ api/app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/images', express.static('images'))
 
-app.use('/', sitemapRoute)
+app.use('/seo', sitemapRoute)
 app.use('/upload', uploadRoutes)
 app.use('/user', userRoutes)
 app.use('/auth', authRoutes)
