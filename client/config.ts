6254f53230b00a9850c6c16cc8c3c30f6c7cf0ca
Ahmed Mahmoud <ahmedmahmoudkzm@gmail.com>
2025-07-18 17:39:51 +0300

    Switching to production

--- client/config.ts
+++ client/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    PRODUCTION: false,
+    PRODUCTION: true,
     REACT_APP_API_URL_PROD: 'https://eight88-backend-service.onrender.com/',
     REACT_APP_API_URL_DEV: 'http://localhost:3000/',
 }
